f8e487923ebe5c17cd65ba18900104967ff573a2
A. Tellez <alsetema@gmail.com>
2020-01-09 22:47:39 +0100

    added external library for java

--- Assignment 3/project/database_workshop_project/src/controller/DatabaseOperations.java
+++ Assignment 3/project/database_workshop_project/src/controller/DatabaseOperations.java
@@ -2,4 +2,8 @@ package controller;
 
 public class DatabaseOperations {
     // will start working on own branch to test communicaitons with sql database
+
+    public DatabaseOperations() {
+
+    }
 }
