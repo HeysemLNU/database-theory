9b938bc8c0cb387ddb0972299d649a927759078c
A. Tellez <alsetema@gmail.com>
2020-01-10 17:15:12 +0100

    managed to fetch information to the database

--- Assignment 3/project/database_workshop_project/src/controller/Controller.java
+++ Assignment 3/project/database_workshop_project/src/controller/Controller.java
@@ -1,4 +1,16 @@
 package controller;
 
+import java.sql.Connection;
+
 public class Controller {
+    DatabaseOperations database = new DatabaseOperations();
+    public Controller() {
+        try {
+            database.connect();
+        } catch (Exception e) {
+            System.err.print(e.getMessage());
+        }
+
+    }
+
 }
--- Assignment 3/project/database_workshop_project/src/controller/DatabaseOperations.java
+++ Assignment 3/project/database_workshop_project/src/controller/DatabaseOperations.java
@@ -1,5 +1,26 @@
 package controller;
 
+import java.sql.*;
+
+
 public class DatabaseOperations {
     // will start working on own branch to test communicaitons with sql database
+    public DatabaseOperations() {
+
+
+    }
+
+    Connection conn;
+    public void connect() throws SQLException {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/reddit2restricted", "albert", "password1");
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("select * from comments");
+
+        while (rs.next()) {
+            System.out.println(rs.getString("id"));
+        }
+
+    }
+
+
 }
