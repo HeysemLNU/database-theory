6e82f10b57e52364f463c87dd06814208212a350
A. Tellez <alsetema@gmail.com>
2020-01-12 00:42:52 +0100

    fixed type errrors

--- Assignment 3/project/database_workshop_project/src/model/DataProcess/InventoryControll.java
+++ Assignment 3/project/database_workshop_project/src/model/DataProcess/InventoryControll.java
@@ -9,116 +9,118 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class InventoryControll {
-    private  boolean idSetable = true;
-    private String barID="bar";
-    private  String wheelsID = "wheels";
-    private  String whandlesID= "window";
-    private  String dhandlesID = "door";
-    private  ArrayList<Bar> bars = new ArrayList<>();
-    private  ArrayList<Wheel> wheels = new ArrayList<>();
-    private  ArrayList<WindowHandle> whandles = new ArrayList<>();
-    private  ArrayList<DoorHandle> dhandles = new ArrayList<>();
-    public  InventoryControll (){}
+    private boolean idSetable = true;
+    private String barID = "bar";
+    private String wheelsID = "wheels";
+    private String whandlesID = "window";
+    private String dhandlesID = "door";
+    private ArrayList<Bar> bars = new ArrayList<>();
+    private ArrayList<Wheel> wheels = new ArrayList<>();
+    private ArrayList<WindowHandle> whandles = new ArrayList<>();
+    private ArrayList<DoorHandle> dhandles = new ArrayList<>();
 
-
-
-    public void addBarsInventory (int amountOfBars, double inputLength,String inputColor, String inputStandard){
-       for (int i = 0; i<amountOfBars; i++){
-           Bars newBar = new Bars(idGenerator(barID),inputLength,inputColor,inputStandard);
-           bars.add(newBar);
-       }
+    public InventoryControll() {
     }
-    public void addWheelsInventory (int amountOfWheels, String inputColor, String inputStandard, int inputType ){
-        for (int i = 0; i<amountOfWheels; i++){
-            Wheels newWheel = new Wheels(idGenerator(wheelsID),inputColor,inputStandard,inputType);
+
+
+    public void addBarsInventory(int amountOfBars, double inputLength, String inputColor, String inputStandard) {
+        for (int i = 0; i < amountOfBars; i++) {
+            Bar newBar = new Bar(idGenerator(barID), inputLength, inputColor, inputStandard);
+            bars.add(newBar);
+        }
+    }
+
+    public void addWheelsInventory(int amountOfWheels, String inputColor, String inputStandard, int inputType) {
+        for (int i = 0; i < amountOfWheels; i++) {
+            Wheel newWheel = new Wheel(idGenerator(wheelsID), inputColor, inputStandard, inputType);
             wheels.add(newWheel);
         }
     }
-    public void addWHandlesInventory (int amountOfWHandles, String inputType, String inputColor, String inputStandard ){
-        for (int i = 0; i<amountOfWHandles; i++){
-            WindowHandles newWHandle = new WindowHandles(inputType,inputColor,inputStandard ,idGenerator(whandlesID));
+
+    public void addWHandlesInventory(int amountOfWHandles, String inputType, String inputColor, String inputStandard) {
+        for (int i = 0; i < amountOfWHandles; i++) {
+            WindowHandle newWHandle = new WindowHandle(inputType, inputColor, inputStandard, idGenerator(whandlesID));
             whandles.add(newWHandle);
         }
     }
-    public void addDHandlesInventory (int amountOfDHandles, String inputType, String inputColor, String inputStandard ){
-        for (int i = 0; i<amountOfDHandles; i++){
-            DoorHandles newDHandle = new DoorHandles(inputType,inputColor,inputStandard ,idGenerator(whandlesID));
+
+    public void addDHandlesInventory(int amountOfDHandles, String inputType, String inputColor, String inputStandard) {
+        for (int i = 0; i < amountOfDHandles; i++) {
+            DoorHandle newDHandle = new DoorHandle(inputType, inputColor, inputStandard, idGenerator(whandlesID));
             dhandles.add(newDHandle);
         }
 
-    public ArrayList<Bar> createBarsInventory (int amountOfBars, double inputLength, String inputColor, String inputStandard){
+
+    }
+    public ArrayList<Bar> createBarsInventory (int amountOfBars, double inputLength, String inputColor, String
+            inputStandard){
         ArrayList<Bar> barsToAdd = new ArrayList<>();
 
 
-
-        return barsToAdd;
-
     }
 
 
-    private int idGenerator(String material){
-        Random randomID = new Random(); // This might be outside the function i will see.
-        if (material == barID){
-            int generatedBarId = randomID.nextInt(99);
-            for (int i=0; i<bars.size();i++){
-                if (bars.get(i).getId()==generatedBarId){
-                    idSetable = false;
+        private int idGenerator (String material){
+            Random randomID = new Random(); // This might be outside the function i will see.
+            if (material == barID) {
+                int generatedBarId = randomID.nextInt(99);
+                for (int i = 0; i < bars.size(); i++) {
+                    if (bars.get(i).getId() == generatedBarId) {
+                        idSetable = false;
+                    }
                 }
-            }
-            if (idSetable == true){
-                return generatedBarId;
-            }else {
-                idSetable =true;
-                idGenerator(material);
-            }
-        }
-        else if (material == wheelsID){
-            int generatedWheelId = randomID.nextInt(99) + 100;
-            for (int i=0; i<wheels.size();i++){
-                if (wheels.get(i).getId()==generatedWheelId){
-                    idSetable = false;
+                if (idSetable == true) {
+                    return generatedBarId;
+                } else {
+                    idSetable = true;
+                    idGenerator(material);
                 }
-            }
-            if (idSetable == true){
-                return generatedWheelId;
-            }else {
-                idSetable =true;
-                idGenerator(material);
-            }
-        }
-        else if (material == whandlesID){
-            int generatedWhandleId = randomID.nextInt(99) + 200;
-            for (int i=0; i<whandles.size();i++){
-                if (whandles.get(i).getId()==generatedWhandleId){
-                    idSetable = false;
+            } else if (material == wheelsID) {
+                int generatedWheelId = randomID.nextInt(99) + 100;
+                for (int i = 0; i < wheels.size(); i++) {
+                    if (wheels.get(i).getId() == generatedWheelId) {
+                        idSetable = false;
+                    }
                 }
-            }
-            if (idSetable == true){
-                return generatedWhandleId;
-            }else {
-                idSetable =true;
-                idGenerator(material);
-            }
-        }
-        else if (material == dhandlesID) {
-            int generatedDhandleId = randomID.nextInt(99) + 300;
-            for (int i=0; i<dhandles.size();i++){
-                if (dhandles.get(i).getId()==generatedDhandleId){
-                    idSetable = false;
+                if (idSetable == true) {
+                    return generatedWheelId;
+                } else {
+                    idSetable = true;
+                    idGenerator(material);
                 }
-            }
-            if (idSetable == true){
-                return generatedDhandleId;
-            }else {
-                idSetable =true;
-                idGenerator(material);
-            }
+            } else if (material == whandlesID) {
+                int generatedWhandleId = randomID.nextInt(99) + 200;
+                for (int i = 0; i < whandles.size(); i++) {
+                    if (whandles.get(i).getId() == generatedWhandleId) {
+                        idSetable = false;
+                    }
+                }
+                if (idSetable == true) {
+                    return generatedWhandleId;
+                } else {
+                    idSetable = true;
+                    idGenerator(material);
+                }
+            } else if (material == dhandlesID) {
+                int generatedDhandleId = randomID.nextInt(99) + 300;
+                for (int i = 0; i < dhandles.size(); i++) {
+                    if (dhandles.get(i).getId() == generatedDhandleId) {
+                        idSetable = false;
+                    }
+                }
+                if (idSetable == true) {
+                    return generatedDhandleId;
+                } else {
+                    idSetable = true;
+                    idGenerator(material);
+                }
 
-        }
+            }
 
             throw new ArithmeticException("Could not Generate a random number !!");
+        }
+        private void getArrays (Bar bars, Wheel wheels, WindowHandle windowHandles, DoorHandle doorHandles){
+            // Albert i need a function from you to fill in these arrays
+        }
     }
-    private void getArrays (Bars bars, Wheels wheels, WindowHandles windowHandles, DoorHandles doorHandles){
-        // Albert i need a function from you to fill in these arrays
-    }
-}
+
