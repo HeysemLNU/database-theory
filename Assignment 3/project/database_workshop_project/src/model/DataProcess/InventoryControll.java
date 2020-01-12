c3480b6e613594125c289de68fcf714cea857b41
HeysemLNU <ab224hv@student.lnu>
2020-01-12 12:45:53 +0100

    user can eddit bars

--- Assignment 3/project/database_workshop_project/src/model/DataProcess/InventoryControll.java
+++ Assignment 3/project/database_workshop_project/src/model/DataProcess/InventoryControll.java
@@ -1,9 +1,9 @@
 package model.DataProcess;
 
-import model.inventory.Bars;
-import model.inventory.DoorHandles;
-import model.inventory.Wheels;
-import model.inventory.WindowHandles;
+import model.inventory.Bar;
+import model.inventory.DoorHandle;
+import model.inventory.Wheel;
+import model.inventory.WindowHandle;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -14,36 +14,82 @@ public class InventoryControll {
     private  String wheelsID = "wheels";
     private  String whandlesID= "window";
     private  String dhandlesID = "door";
-    private  ArrayList<Bars> bars = new ArrayList<>();
-    private  ArrayList<Wheels> wheels = new ArrayList<>();
-    private  ArrayList<WindowHandles> whandles = new ArrayList<>();
-    private  ArrayList<DoorHandles> dhandles = new ArrayList<>();
-    public  InventoryControll (){}
+    private  ArrayList<Bar> bars = new ArrayList<>();
+    private  ArrayList<Wheel> wheels = new ArrayList<>();
+    private  ArrayList<WindowHandle> whandles = new ArrayList<>();
+    private  ArrayList<DoorHandle> dhandles = new ArrayList<>();
+    public  InventoryControll (){
+
+    }
 
 
     public void addBarsInventory (int amountOfBars, double inputLength,String inputColor, String inputStandard){
        for (int i = 0; i<amountOfBars; i++){
-           Bars newBar = new Bars(idGenerator(barID),inputLength,inputColor,inputStandard);
+           Bar newBar = new Bar(idGenerator(barID),inputLength,inputColor,inputStandard);
            bars.add(newBar);
        }
     }
     public void addWheelsInventory (int amountOfWheels, String inputColor, String inputStandard, int inputType ){
         for (int i = 0; i<amountOfWheels; i++){
-            Wheels newWheel = new Wheels(idGenerator(wheelsID),inputColor,inputStandard,inputType);
+            Wheel newWheel = new Wheel(idGenerator(wheelsID),inputColor,inputStandard,inputType);
             wheels.add(newWheel);
         }
     }
     public void addWHandlesInventory (int amountOfWHandles, String inputType, String inputColor, String inputStandard ){
         for (int i = 0; i<amountOfWHandles; i++){
-            WindowHandles newWHandle = new WindowHandles(inputType,inputColor,inputStandard ,idGenerator(whandlesID));
+            WindowHandle newWHandle = new WindowHandle(inputType,inputColor,inputStandard ,idGenerator(whandlesID));
             whandles.add(newWHandle);
         }
     }
     public void addDHandlesInventory (int amountOfDHandles, String inputType, String inputColor, String inputStandard ){
         for (int i = 0; i<amountOfDHandles; i++){
-            DoorHandles newDHandle = new DoorHandles(inputType,inputColor,inputStandard ,idGenerator(whandlesID));
+            DoorHandle newDHandle = new DoorHandle(inputType,inputColor,inputStandard ,idGenerator(whandlesID));
             dhandles.add(newDHandle);
         }
+    }
+    private   void removeFromInvetoryBar(int idToRemove){
+        // The user will input the id of the bar that he wants to remove from the table
+    }
+    public  void removeFromInventoryWheels(int amountOfWheels, String inputColor, String inputStandard, int inputType){
+        // This will make the user remove wheels depending on the color,
+    }
+    public  void removeFromInventoryWHandles(int amountOfWHandles, String inputType, String inputColor, String inputStandard ){
+
+    }
+    public void removeFromInventoryDHandles (String inputType, String inputColor, String inputStandard){
+        // I dont think we need to specifie the amount since it would probably be one at a time
+    }
+    public  void editBar(int id, double amountUsed){
+        // Albert I was thinking that we would display all the bars with their properties and stuff and then he can
+        // just give us the id of it and how much he used. Or we can do it both ways where he just enters the properties and the amount
+        // and we just minus from that
+        for (int i=0; i<bars.size(); i++){
+            if (bars.get(i).getId()==id){
+                if (bars.get(i).getLength()>=amountUsed){
+                    bars.get(i).setLength(bars.get(i).getLength()-amountUsed);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    public  void editBarMain (double inputLengthUsed,String inputColor, String inputStandard){
+        for (int i=0; i<bars.size(); i++){
+           if ((bars.get(i).getColor()==inputColor)&&(bars.get(i).getStandard()==inputStandard)){
+               if (bars.get(i).getLength()>=inputLengthUsed){
+                   bars.get(i).setLength(bars.get(i).getLength()-inputLengthUsed);
+                   if (bars.get(i).getLength()==0.0){
+                       removeFromInvetoryBar(bars.get(i).getId());
+                       // function to remove that bar from database
+                   }
+
+                   // A function to save the changes
+                   break;
+               }
+           }
+        }
+
     }
 
 
@@ -109,7 +155,7 @@ public class InventoryControll {
 
             throw new ArithmeticException("Could not Generate a random number !!");
     }
-    private void getArrays (Bars bars, Wheels wheels, WindowHandles windowHandles, DoorHandles doorHandles){
+    private void getArrays (Bar bar, Wheel wheels, WindowHandle windowHandles, DoorHandle doorHandles){
         // Albert i need a function from you to fill in these arrays
     }
 }
--- Assignment 3/project/database_workshop_project/src/model/inventory/Bar.java
+++ Assignment 3/project/database_workshop_project/src/model/inventory/Bar.java
@@ -1,43 +1,42 @@
 package model.inventory;
 
-public class Wheels {
+public class Bar {
     private int id;
+    private double length;
+    private String color;
     private  String standard;
-    private  String color;
-    private int type;
 
-    public  Wheels (int inputID, String inputColor, String inputStandard, int inputType ){
+    public Bar(int inputID, double inputLength, String inputColor, String inputStandard ){
         setId(inputID);
         setColor(inputColor);
         setStandard(inputStandard);
-        setType(inputType);
+        setLength(inputLength);
     }
     public void setId(int inputID){
         id = inputID;
     }
+    public void setLength(double inputLength){
+        length = inputLength;
+    }
     public void setColor (String inputColor){
-        color = inputColor;
+     color = inputColor;
     }
     public void  setStandard (String inputStandard){
         standard = inputStandard;
     }
-    public void  setType (int inputType){
-        type = inputType;
-    }
-
-    public int getId() {
+    public int getId (){
         return id;
     }
 
-    public String getStandard() {
-        return standard;
+    public double getLength() {
+        return length;
     }
 
     public String getColor() {
         return color;
     }
 
-    public int getType() {
-        return type;
+    public String getStandard() {
+        return standard;
     }
 }
