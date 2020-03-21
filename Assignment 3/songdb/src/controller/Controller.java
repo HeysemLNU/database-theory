e31f49dc045cd4cb43eed94a3b021ffa5f8fe9de
A. Tellez <alsetema@gmail.com>
2020-03-21 22:26:44 +0100

    does this work?

--- Assignment 3/songdb/src/controller/Controller.java
+++ Assignment 3/songdb/src/controller/Controller.java
@@ -16,24 +16,38 @@ public class Controller {
 
         Statement st = con.createStatement();
 
+        st.executeUpdate("DROP TABLE IF EXISTS Songs,Artists,Albums");
+
         st.executeUpdate("CREATE TABLE IF NOT EXISTS Artists " +
                 "(ArtistID int NOT NULL AUTO_INCREMENT," +
                 "Nationality varchar(50)," +
                 "Name varchar(50)," +
                 "PRIMARY KEY (ArtistID))");
+        System.out.println("this is here now");
+        //finish the ttable creation entry
+        st.executeUpdate("CREATE TABLE IF NOT EXISTS Albums " +
+                "(AlbumID int NOT NULL AUTO_INCREMENT," +
+                "Artist int," +
+                "Name varchar(100)," +
+                "Year int," +
+                "RecordLabel varchar(100)," +
+                "FOREIGN KEY(Artist) REFERENCES Artists(ArtistID)," +
+                "PRIMARY KEY (AlbumID))");
+        System.out.println("this is here now again");
+        st.executeUpdate("CREATE TABLE IF NOT EXISTS Songs " +
+                "(SongID int NOT NULL AUTO_INCREMENT," +
+                "Length int," +
+                "Lyrics text," +
+                "Year int," +
+                "Name varchar (200)," +
+                "Artist int," +
+                "Album int," +
+                "PRIMARY KEY (SongID)," +
+                "FOREIGN KEY (Artist) REFERENCES Artists(ArtistID)," +
+                "FOREIGN KEY (Album) REFERENCES Albums(AlbumID))");
         con.close();
 
-        /*   ResultSet result = st.executeQuery("CREATE TABLE songs (" +
-                    "SongID int NOT NULL AUTO_INCREMENT," +
-                    "Length int," +
-                    "Lyrics text," +
-                    "Year int," +
-                    "NAME varchar 200," +
-                    "Artist int," +
-                    "Album int," +
-                    "PRIMARY KEY (SongsID)," +
-                    "FOREIGN KEY (Artist) REFERENCES Artists(ArtistID)" +
-                    "FOREIGN KEY (Album) REFERENCES Albums(AlbumID))");*/
+
     }
 
 
