16e75714d6d6e166e590e1d4197a59d411db3c94
HeysemLNU <ab224hv@student.lnu>
2020-03-25 15:31:05 +0100

    done with add and delete

--- Assignment 3/songdb/src/Main.java
+++ Assignment 3/songdb/src/Main.java
@@ -1,5 +1,8 @@
 import controller.*;
 import model.ConnectionParams;
+import model.CountryCode;
+import model.dbElements.Album;
+import model.dbElements.Artist;
 import model.dbElements.Song;
 
 import java.sql.SQLException;
@@ -11,6 +14,19 @@ public class Main {
 	// write your code here
         Controller con = new Controller();
         ConnectionParams cp = new ConnectionParams("desktop.noxel.tk",42069,"database", "animewaifu","songs");
+        //Trial
+        CountryCode cc = CountryCode.AE;
+        Artist newArtist = new Artist("Major", cc);
+        Album newAlbum = new Album("Cool Abum",2003,2,"New Paramaount");
+        Song newSong = new Song(420,"WorkIT",2005,2,1,"This is my Song Hooollllaaaa");
+        EditDB editDB = new EditDB();
+        editDB.addNewArtist(newArtist,cp);
+        editDB.addNewAlbum(newAlbum,cp);
+        editDB.addNewSong(newSong,cp);
+        //editDB.deleteSong(1,cp);
+        //editDB.deleteAlbum(2,cp);
+        editDB.deleteArtist(1,cp);
+        //
 
         try {
             con.startView();
--- Assignment 3/songdb/src/controller/EditDB.java
+++ Assignment 3/songdb/src/controller/EditDB.java
@@ -8,23 +8,43 @@ import model.dbElements.Song;
 import java.sql.*;
 
 public class EditDB {
-    ConnectionParams params = null;
+   // ConnectionParams params = null;
     public EditDB (){}
 
-    public void  addNewArtist (Artist newArtist) throws SQLException {
+    public void  addNewArtist (Artist newArtist, ConnectionParams params) throws SQLException {
         Connection con = DriverManager.getConnection(params.getNoCreds(), params.getUsername(), params.getPassword());
         Statement addArtist = con.createStatement();
         addArtist.executeUpdate("INSERT INTO Artists (Nationality, Name) VALUES ('"+newArtist.getNationality()+"', '"+newArtist.getName()+"')");
         con.close();
     }
-    public void addNewSong (Song newSong) throws SQLException {
+    public void addNewSong (Song newSong, ConnectionParams params) throws SQLException {
         Connection con = DriverManager.getConnection(params.getNoCreds(), params.getUsername(), params.getPassword());
         Statement addSong = con.createStatement();
         addSong.executeUpdate("INSERT INTO Songs (Length, Lyrics, Year, Name, Artist, Album) VALUES ('"+newSong.getLength()+"',  '"+newSong.getLyricks()+"','"+newSong.getYear()+"','"+newSong.getName()+"','"+newSong.getArtistID()+"','"+newSong.getAlbumID()+"')");
+        con.close();
     }
-    public void addNewAlbum (Album newAlbum) throws SQLException {
+    public void addNewAlbum (Album newAlbum, ConnectionParams params) throws SQLException {
         Connection con = DriverManager.getConnection(params.getNoCreds(), params.getUsername(), params.getPassword());
         Statement addAlbum = con.createStatement();
         addAlbum.executeUpdate("INSERT  INTO Albums (Artist, Name, Year, RecordLabel) VALUES ('"+newAlbum.getArtistID()+"','"+newAlbum.getName()+"','"+newAlbum.getYear()+"','"+newAlbum.getRecordLabel()+"')");
+        con.close();
+    }
+    public void deleteArtist (int artistID, ConnectionParams params) throws SQLException {
+        Connection con = DriverManager.getConnection(params.getNoCreds(), params.getUsername(), params.getPassword());
+        Statement delete = con.createStatement();
+        delete.executeUpdate("DELETE FROM Artists WHERE ArtistID = '"+artistID+"'");
+        con.close();
+    }
+    public void deleteSong (int songID, ConnectionParams params) throws SQLException {
+        Connection con = DriverManager.getConnection(params.getNoCreds(), params.getUsername(), params.getPassword());
+        Statement delete = con.createStatement();
+        delete.executeUpdate("DELETE FROM Songs WHERE SongID = '"+songID+"'");
+        con.close();
+    }
+    public void deleteAlbum (int albumID, ConnectionParams params) throws SQLException {
+        Connection con = DriverManager.getConnection(params.getNoCreds(), params.getUsername(), params.getPassword());
+        Statement delete = con.createStatement();
+        delete.executeUpdate("DELETE FROM Albums WHERE AlbumID = '"+albumID+"'");
+        con.close();
     }
 }
